0286fd70088bdc3adeb1521115ba12ef5af2167a
Gokul-C <gokulc741@gmail.com>
2022-03-28 14:54:57 +0530

    multiple-ec2-deploy

--- deploy-multiple-ec2.tf
+++ deploy-multiple-ec2.tf
@@ -21,6 +21,9 @@ variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
 variable  "private_key_location"{}
+variable "ec2-count" {} 
+  
+
 
 //Creating vpc ----1
 
@@ -31,7 +34,7 @@ resource "aws_vpc" "myapp-vpc" {
     } 
 }
 
-//Creating subnet ----2
+//Creating subnet-1 ----2
 
 resource "aws_subnet" "myapp-subnet-1" {     
    vpc_id = aws_vpc.myapp-vpc.id
@@ -41,6 +44,17 @@ resource "aws_subnet" "myapp-subnet-1" {
       Name: "${var.env_prefix}-subnet-1"
     }
 }
+
+/*//Creating subnet-2 ----2
+
+resource "aws_subnet" "myapp-subnet-2" {     
+   vpc_id = aws_vpc.myapp-vpc.id
+   cidr_block = var.subnet1-cidr_block             
+    availability_zone = "ap-south-1a"      
+    tags = {
+      Name: "${var.env_prefix}-subnet-2"
+    }
+}*/
 
 //Creating gateway   ----3
 
@@ -125,7 +139,7 @@ data "aws_ami" "latest-amazon-linux-image" {
   
 }
 
-//shows ami id for confirmation
+/*//shows ami id for confirmation
 
 output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
@@ -133,7 +147,9 @@ output "aws_ami_id" {
 
 output "ec2_public_ip" {
   value = aws_instance.myapp-server.public_ip
-}
+}*/
+
+// Inserting local ssh key pair into server
 
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"
@@ -149,6 +165,10 @@ resource "aws_instance" "myapp-server" {
   instance_type = var.instance_type
 
   subnet_id = aws_subnet.myapp-subnet-1.id
+
+  count = var.ec2-count  //count of an ec2 instances.
+ 
+
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone = var.avail_zone
   
@@ -161,10 +181,9 @@ resource "aws_instance" "myapp-server" {
 
 
   tags = {
-    Name = "${var.env_prefix}-server"
-    //foo = "bar"
+    Name = "${var.env_prefix}-${count.index+1}-server"
+    foo = "bar"   // Bash script will execute on all machines
   }
   
   
 }
-
